064bb41a557f9049de2eaae0aa47af028d951a93
Yana Ustsinovich <yustinovich@icloud.com>
2019-04-25 05:11:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distanceFeet) {
+  let driverMessage
+  if (distanceFeet <= 400) {
+    driverMessage = "This one is on me!"
+  }
+  return driverMessage;
+    if (distanceFeet >= 2000 ) {
+      driverMessage = "I will gladly take your thirty bucks."
+    }
+}
