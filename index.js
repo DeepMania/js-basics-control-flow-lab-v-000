faef4643a56362f52e3abe258844701af85986c0
Yana Ustsinovich <yustinovich@icloud.com>
2019-04-25 05:27:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function scuberGreetingForFeet(distanceFeet) {
 
 function ternaryCheckCity (city) {
   let driverMessage;
-  city = 'NYC' ? driverMessage = "Ok, sounds good." : driverMessage = "No go."
+  city = 'NYC' ? (driverMessage = "Ok, sounds good.") : (driverMessage = "No go.")
 
   return driverMessage;
 }
